cd66d5d634446bdaa86fc01c759223f04f6e55af
AjayS80 <ajaysheokand80@gmail.com>
2022-01-25 04:00:07 +0000

    RMS123 changes made to steps

--- src/test/java/cucumber/steps/PostWithParameter.java
+++ src/test/java/cucumber/steps/PostWithParameter.java
@@ -8,39 +8,39 @@ import io.cucumber.java.en.When;
 public class PostWithParameter {
     @Given("^The link to test$")
     public void the_link_to_test() throws Throwable {
-        System.out.println("^The link to test$");
+        System.out.println("^11111");
     }
 
     @When("^I type the \"([^\"]*)\" in the box$")
     public void i_type_the_something_in_the_box(String strArg1) throws Throwable {
-        System.out.println("^I type the \"([^\"]*)\" in the box$");
+        System.out.println("22222");
     }
 
 
 
     @Then("^the message should get posted$")
     public void the_message_should_get_posted() throws Throwable {
-        System.out.println("^the message should get posted$");
+        System.out.println("3333");
     }
 
     @Then("^the  link should be opened$")
     public void the_link_should_be_opened() throws Throwable {
-        System.out.println("^the  link should be opened$");
+        System.out.println("77777");
     }
 
     @And("^Click on the Post button$")
     public void click_on_the_post_button() throws Throwable {
-        System.out.println("^Click on the Post button$");
+        System.out.println("44444");
     }
 
     @And("^Click on the okay button$")
     public void click_on_the_okay_button() throws Throwable {
-        System.out.println("^Click on the Post button$");
+        System.out.println("55555");
     }
 
 
     @When("I on the given {string} link")
     public void iOnTheGivenLink(String arg0) {
-        System.out.println("google");
+        System.out.println(66666);
     }
 }
